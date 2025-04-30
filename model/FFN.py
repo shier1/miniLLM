cf7bebe4b71b6db11254d188e869116a05734567
shier1 <zyanping14@gmail.com>
2025-04-30 20:32:31 +0800

    add the important submodule

--- model/FFN.py
+++ model/FFN.py
@@ -1,22 +1,27 @@
 import torch.nn as nn
-
-
-
-class FFN(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-
-    def forward(self, x):
-        out = x
-        return out
+import torch.nn.functional as F
 
 
 class MLP(nn.Module):
-    def __init__(self, config):
+    def __init__(self, 
+                 hidden_size, 
+                 intermediate_size, 
+                 mlp_bias=False, 
+                 mlp_dropout=0.3,
+                 **keywargs):
+        
         super().__init__()
-
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=mlp_bias)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=mlp_bias)
+        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=mlp_bias)
+        self.dropout = nn.Dropout(mlp_dropout)
+        
     def forward(self, x):
-        out = x
-        return out
+        down_proj = self.dropout(self.down_proj(F.silu(self.gate_proj(x)) * self.up_proj(x)))
+        return down_proj
 
- 
+
+def get_ffn(config):
+    return eval(config.FFN.name)(**config.FFN)
--- model/attention.py
+++ model/attention.py
@@ -1,25 +1,145 @@
+import math
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
+def rotate_half(x):
+    x1, x2 = x.chunk(2, dim=-1)
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotate_pos_emb(q, k, cos, sin, unsqueeze_dim=2):
+    
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+   
+    q_embed = (q*cos) + (rotate_half(q)*sin)
+    k_embed = (k*cos) + (rotate_half(k)*sin)
+    
+    return q_embed, k_embed
+
+
+class RotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_seq_len=1024):
+        super(RotaryEmbedding, self).__init__()
+        self.dim = dim
+        self.max_seq_len = max_seq_len
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+        t = torch.arange(max_seq_len).float().unsqueeze(1)
+        freqs = t @ inv_freq.unsqueeze(0)
+        freqs = torch.cat((freqs, freqs), dim=-1)
+        
+        self.register_buffer("cos_cached", freqs.cos())
+        self.register_buffer("sin_cached", freqs.sin())
+        
+    def forward(self, q, k):
+        cos = self.cos_cached[:q.shape[1], :].unsqueeze(0)
+        sin = self.sin_cached[:q.shape[1], :].unsqueeze(0)
+        return apply_rotate_pos_emb(q, k, cos, sin)
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, config):
+    def __init__(self, hidden_size, eps=1e-6):
+        
         super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        hidden_states = hidden_states.float()
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        return self.weight * hidden_states.float()
+
     
-    def forward(self, x):
-        out = x
-        return out
-    
+def repeat_kv(hidden_states, n_rep):
+    batch, slen, num_key_value_heads, head_dim = hidden_states.shape
+    if n_rep == 1:
+        return hidden_states
+    hidden_states = hidden_states[:, :, :, None, :].expand(batch, slen, num_key_value_heads, n_rep, head_dim)
+    return hidden_states.reshape(batch, slen, num_key_value_heads * n_rep, head_dim)
+
 
 class GQAttention(nn.Module):
-    def __init__(self, config):
+    def __init__(self,
+                 max_seq_len,
+                 attention_dropout,
+                 hidden_size,
+                 num_attention_heads,
+                 num_key_value_heads,
+                 flash_attn,
+                 attention_bias,
+                 head_dim=None,
+                 **kwargs):
         super().__init__()
+        self.max_seq_len = max_seq_len
+        self.dropout = attention_dropout
+        self.hidden_size = hidden_size
+        self.num_heads = num_attention_heads
+        self.head_dim = self.hidden_size // self.num_heads if head_dim is None else head_dim
+        self.num_key_value_heads = num_key_value_heads
+        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
+        self.k_cache, self.v_cache = None, None
+        self.is_causal = True
+        self.flash_attn = flash_attn
 
-    
-    def _init_tensor(self):
-        return
+        self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=attention_bias)
+        self.k_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=attention_bias)
+        self.v_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=attention_bias)
+        self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias=attention_bias)
+        self.residual_dropout = nn.Dropout(self.dropout)
+        self.attention_dropout = nn.Dropout(self.dropout)
+        self.rotary_emb = RotaryEmbedding(self.head_dim)
 
-    def forward(self, x):
-        out = x
-        return out
-    
+
+    def forward(self, hidden_states, use_kv_cache=False):
+        b, s = hidden_states.shape[:2]
+        if use_kv_cache and self.eval():
+            if self.k_cache is None or self.k_cache.shape[1] != s-1:
+                q, k, v = self.q_proj(hidden_states), self.k_proj(hidden_states), self.v_proj(hidden_states)
+            else:
+                token = hidden_states[:, -1:, :]
+                q = torch.cat((torch.zeros_like(hidden_states[:, :-1, :]), self.q_proj(token)), dim=1)
+                k = torch.cat((self.k_cache, self.k_proj(token)), dim=1)
+                v = torch.cat((self.v_cache, self.v_proj(token)), dim=1)
+            self.k_cache, self.v_cache = k, v
+        else:
+            q, k, v = self.q_proj(hidden_states), self.k_proj(hidden_states), self.v_proj(hidden_states)
+            
+        q = q.view(b, s, self.num_heads, self.head_dim)
+        k = k.view(b, s, self.num_key_value_heads, self.head_dim)
+        v = v.view(b, s, self.num_key_value_heads, self.head_dim)
+        
+        q, k = self.rotary_emb(q, k)
+        
+        k = repeat_kv(k, self.num_key_value_groups)
+        v = repeat_kv(v, self.num_key_value_groups)
+        
+        q = q.transpose(1, 2) # b, self.num_heads, s, self.head_dim
+        k = k.transpose(1, 2) # b, self.num_heads, s, self.head_dim
+        v = v.transpose(1, 2) # b, self.num_heads, s, self.head_dim
+        
+        if self.flash_attn:
+            # q*k转置，（b, self.num_heads, s, self.head_dim）* (b, self.num_heads, self.head_dim，s) = （b, self.num_heads, s, s）
+            # q*k/sqrt(self.head_dim)*v  （b, self.num_heads, s, s）* (b, self.num_heads, s, self.head_dim) = b, self.num_heads, s, self.head_dim
+            output = F.scaled_dot_product_attention(q, k, v, attn_mask=None, 
+                                                    dropout_p=self.dropout if self.training else 0.0, 
+                                                    is_causal=self.is_causal) 
+        else:
+            mask = torch.full((1, 1, self.max_seq_len, self.max_seq_len), float("-inf"))  # 初始化掩码
+            mask = torch.triu(mask, diagonal=1)  # 生成上三角掩码
+            scores = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.head_dim)  # 计算注意力分数
+            scores = scores + self.mask[:, :, :s, :s]  # 应用掩码
+            scores = F.softmax(scores.float(), dim=-1).type_as(q)  # 计算 softmax
+            scores = self.attention_dropout(scores)  # 应用注意力 dropout
+            output = torch.matmul(scores, v)  # 计算输出
+        
+        output = output.transpose(1, 2).contiguous().view(b, s, -1) # b, s, self.hidden_size
+        
+        output = self.o_proj(output)
+        output = self.residual_dropout(output)
+        return output
+
+
+def get_attention(config):
+    return eval(config.attention.name)(**config.attention)
